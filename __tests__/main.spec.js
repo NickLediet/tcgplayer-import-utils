c32f8458705d75630ccd0e4e540499aa984edd72
Nicholas Lediet <nlediet@gmail.com>
2022-10-02 02:34:26 -0400

    Implement & test object extraction from import strings

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,9 +1,71 @@
-import {importList} from '../main';
+import {importList, extractCard} from '../main';
 
-describe('main.js', () => {
-    it('should return true', () => {
-        const value = importList();
+describe('importList()', () => {
+    it('it should create a list of cards', () => {
+        const list = `
+            TOTAL: 236 cards - $1,547.30 (3 cards missing price)
+            1 Ugin, the Ineffable (JP Alternate Art) [WAR]
+            1 Tamiyo, Collector of Tales (JP Alternate Art) [WAR]
+            1 Nissa, Who Shakes the World (JP Alternate Art) [WAR]
+        `;
 
-        expect(value).toBe(true);
+        const value = importList(list);
+        expect(value.length).toBe(3);
     });
+
+    it('should not return items with whitespace in their names', () => {
+        const list = `
+            TOTAL: 236 cards - $1,547.30 (3 cards missing price)
+            1 Ugin, the Ineffable (JP Alternate Art) [WAR]
+            1 Tamiyo, Collector of Tales (JP Alternate Art) [WAR]
+            1 Nissa, Who Shakes the World (JP Alternate Art) [WAR]
+        `;
+
+        const expectedList = [
+            'Ugin, the Ineffable',
+            'Tamiyo, Collector of Tales',
+            'Nissa, Who Shakes the World'
+        ];
+
+        const importedList = importList(list);
+
+        importedList.forEach((importItem, index) => {
+            expect(importItem.name).toStrictEqual(expectedList[index]);
+        });
+    });
+
+   it('should return a list of objects with the correct properties', () => {
+        const list = `
+            TOTAL: 236 cards - $1,547.30 (3 cards missing price)
+            1 Ugin, the Ineffable (JP Alternate Art) [WAR]
+            1 Tamiyo, Collector of Tales (JP Alternate Art) [WAR]
+            1 Nissa, Who Shakes the World (JP Alternate Art) [WAR]
+        `;
+
+        const importItems = importList(list);
+        
+        importItems.forEach(importItem => {
+            expect(importItem.hasOwnProperty('quantity')).toStrictEqual(true);
+            expect(importItem.hasOwnProperty('name')).toStrictEqual(true);
+            expect(importItem.hasOwnProperty('frame')).toStrictEqual(true);
+            expect(importItem.hasOwnProperty('setCode')).toStrictEqual(true);
+        });
+   }) 
+
 });
+
+describe('extractImportLine()', () => {
+    it('should return return a card', () => {
+        const importLine = '1 Ugin, the Ineffable (JP Alternate Art) [WAR]';
+        const expectedImport = 
+            {
+                quantity: 1,
+                name: 'Ugin, the Ineffable',
+                frame: 'JP Alternate Art',
+                setCode: 'WAR'
+            };
+        const result = extractCard(importLine);
+
+        expect(result).toEqual(expectedImport);
+    })
+})
--- main.js
+++ main.js
@@ -1,4 +1,57 @@
+export const SET_REGEX = /\s\[(.*)\]$/;
+export const FRAME_QUANTITY = /\s\((.*)\)$/;
+export const QUANTITY_REGEX = /^(\d)\s/;
 
-export const importList = () => {
-    return true;
+export const extractCard = (importLine) => {
+    const fields = [
+        {
+            key: 'setCode',
+            regex: SET_REGEX
+        },
+        {
+            key: 'frame',
+            regex: FRAME_QUANTITY
+        },
+        {
+            key: 'quantity',
+            regex: QUANTITY_REGEX,
+            value: (matches) => parseInt(matches[1])
+        },
+        {
+            key: 'name',
+            regex: /.*/,
+            value: matches => matches[0]
+        }
+    ];
+
+    const card = fields.reduce(
+        (cardAcc, field) => {
+            let matches = cardAcc.importLine.match(field.regex);
+            cardAcc[field.key] = typeof field.value === 'function' ? 
+                field.value(matches):
+                matches[1];
+
+            cardAcc.importLine = cardAcc.importLine.replace(matches[0], '');
+
+            return cardAcc;
+        },
+        { importLine }
+    );
+    delete card.importLine;
+
+    return card;
+}
+
+export const importList = (importString) => {
+    const imports = importString.split('\n')
+        .filter(line => /^\s*\d/.test(line))
+        .map(line => extractCard(line.trim()));
+    return imports;
 };
+
+/**
+ * 
+ * POC workflow
+ * - load an array of extracted cards
+ * - make a scryfall query with the setCode and 
+ */
